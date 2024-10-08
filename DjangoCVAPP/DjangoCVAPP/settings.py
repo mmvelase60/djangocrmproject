ff7f80d7af8ba05e49b4258646feacd0f237cb6c
Mthokozisi <mmvelase60@gmail.com>
2024-10-08 19:32:44 +0200

    small change

--- DjangoCVAPP/DjangoCVAPP/settings.py
+++ DjangoCVAPP/DjangoCVAPP/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cvexcel'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,19 @@ WSGI_APPLICATION = 'DjangoCVAPP.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'localhost:1521/xe',
+        'USER': 'system',
+        'PASSWORD': 'orcl',
     }
 }
 
