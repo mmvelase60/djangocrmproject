3d90342129542c7438dda0fe117438201bc39ded
Mthokozisi <mmvelase60@gmail.com>
2024-10-09 15:40:04 +0200

    Export CV in Excel & CSV

--- DjangoCVAPP/DjangoCVAPP/settings.py
+++ DjangoCVAPP/DjangoCVAPP/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 'cvexcel.apps.MainConfig',
     'cvexcel',
+     'import_export'
 ]
 
 MIDDLEWARE = [
--- DjangoCVAPP/cvexcel/admin.py
+++ DjangoCVAPP/cvexcel/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Record
 # Register your models here.
-# from import_export.admin import ImportExportModelAdmin
-class RecordAdmin(admin.ModelAdmin):
+from import_export.admin import ImportExportModelAdmin
+class RecordAdmin(ImportExportModelAdmin):
     list_display=['id','created_at','first_name','last_name','email','phone','city','state']
 
-admin.site.register(Record,RecordAdmin)
+admin.site.register(Record,RecordAdmin)    
